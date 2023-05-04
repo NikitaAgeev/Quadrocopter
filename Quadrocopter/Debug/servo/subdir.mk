b448fba8c5ca33cf4bb0687b02bedb07c11084f7
NA <nik21032004@gmail.com>
2023-05-05 01:50:32 +0300

    save

---
+++ Quadrocopter/Debug/servo/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../servo/servo.cpp 
+
+OBJS += \
+./servo/servo.o 
+
+CPP_DEPS += \
+./servo/servo.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+servo/%.o servo/%.su: ../servo/%.cpp servo/subdir.mk
+	arm-none-eabi-g++ "$<" -mcpu=cortex-m3 -std=gnu++14 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F100xB -c -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -fno-exceptions -fno-rtti -fno-use-cxa-atexit -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-servo
+
+clean-servo:
+	-$(RM) ./servo/servo.d ./servo/servo.o ./servo/servo.su
+
+.PHONY: clean-servo
+
